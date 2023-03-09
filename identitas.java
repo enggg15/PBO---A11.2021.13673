8bff0a3cd83857ddc46b565b637e720149a5bbfa
Nicola Bias Wisangeni <114697800+enggg15@users.noreply.github.com>
2023-03-09 21:44:07 +0700

    Add files via upload

---
+++ identitas.java
@@ -0,0 +1,15 @@
+
+/*public class identitas{
+    public static void main(String[] args){
+        System.out.print ("A11.2021.13673 . Nicola Bias Wisanggeni");
+       */
+	public class identitas{
+    public static void main(String[] args){
+        String nama = "Nicola";
+        System.out.println("halo, nama saya adalah "+nama );
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.print("Saya sedang menulis program java");
+        
+
+    }
+}
